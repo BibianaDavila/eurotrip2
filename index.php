b7c6ac4306e6c997cd15e47ea2be2855b64db59c
BibianaDavila <00265176@ufrgs.br>
2017-11-29 10:37:54 -0200

    +

--- index.php
+++ index.php
@@ -546,7 +546,7 @@
 						<a class="gallery-img image-popup image-popup" href="images/pictures/23627371_2212059428820442_881350606_o.jpg"><img src="images/pictures/23627371_2212059428820442_881350606_o.jpg" class="img-responsive" alt=""></a>
 					</div>
 					<div class="gallery animate-box">
-						<a class="gallery-img image-popup image-popup" href="images/pictures/IMG_0122_view.jpg"><img src="images/pictures/IMG_0122_view.jpg" class="img-responsive" alt=""></a>
+						<a class="gallery-img image-popup image-popup" href="images/pictures/IMG_0122_view.JPG"><img src="images/pictures/IMG_0122_view.JPG" class="img-responsive" alt=""></a>
 					</div>
 					<div class="gallery animate-box">
 						<a class="gallery-img image-popup image-popup" href="images/pictures/23698684_2212049295488122_1031862141_o.jpg"><img src="images/pictures/23698684_2212049295488122_1031862141_o.jpg" class="img-responsive" alt=""></a>
@@ -554,7 +554,7 @@
 				</div>
 				<div class="col-md-3 col-sm-6">
 					<div class="gallery animate-box">
-						<a class="gallery-img image-popup image-popup" href="images/pictures/IMG_2425.jpg"><img src="images/pictures/IMG_2425.jpg" class="img-responsive" alt=""></a>
+						<a class="gallery-img image-popup image-popup" href="images/pictures/IMG_2425.JPG"><img src="images/pictures/IMG_2425.JPG" class="img-responsive" alt=""></a>
 					</div>
 					<div class="gallery animate-box">
 						<a class="gallery-img image-popup image-popup" href="images/pictures/23659887_2212059218820463_2113900419_o.jpg"><img src="images/pictures/23659887_2212059218820463_2113900419_o.jpg" class="img-responsive" alt=""></a>
